a1f8bcc7088abb870d1ec8f8eb0e10fb67d87e23
Ruth Temporal <ruthtemp@gmail.com>
2022-10-20 17:15:40 +0200

    fixed distorted image

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,73 @@
-import React, { useState } from 'react';
-import { Alert, Col, Container, Row } from 'react-bootstrap';
-import { FoodForm } from './FoodForm';
-import { SymptomsComp } from './Symptoms';
-import { Calendar } from './Calendar';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from "react";
+import { Alert, Col, Container, Row } from "react-bootstrap";
+import { FoodForm } from "./FoodForm";
+import { SymptomsComp } from "./Symptoms";
+import { Calendar } from "./Calendar";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
-import { NavigationBar } from './NavigationBar';
-import { Home } from './Home';
-
-
+import "./App.css";
+import { NavigationBar } from "./NavigationBar";
+import { Home } from "./Home";
 
 export interface Food {
-  type: "food",
-  date: Date,
-  meal: string,
-  included: string[],
-  foodList: string[],
-  selectedTime: string,
-  taste: number,
-  quality: number,
-  quantity: number,
-  overallExp: number
+  type: "food";
+  date: Date;
+  meal: string;
+  included: string[];
+  foodList: string[];
+  selectedTime: string;
+  taste: number;
+  quality: number;
+  quantity: number;
+  overallExp: number;
 }
 
 export interface Symptoms {
-  type: "symptoms",
-  date: Date,
-  physical: string[],
-  mood: string[],
-  comments: ""
+  type: "symptoms";
+  date: Date;
+  physical: string[];
+  mood: string[];
+  comments: "";
 }
 
-export type Answer = Food | Symptoms
+export type Answer = Food | Symptoms;
 
+function App(p: { savedAnswers: Answer[] }) {
+  const [alert, setAlert] = useState(false);
 
-
-
-function App(p: {
-  savedAnswers: Answer[]
-}) {
-
-  const [alert, setAlert] = useState(false)
-
-  const [answers, setAnswers] = useState<Answer[]>(p.savedAnswers)
+  const [answers, setAnswers] = useState<Answer[]>(p.savedAnswers);
 
   const addAnswer = (answer: Answer) => {
-    const newAnswers = answers.concat(answer)
-    setAnswers(newAnswers) //update the state
-    localStorage.setItem("answers", JSON.stringify(newAnswers)) // save the whole state in localstorage (newAnswers). "answers" is stale - not yet updated state
-  }
+    const newAnswers = answers.concat(answer);
+    setAnswers(newAnswers); //update the state
+    localStorage.setItem("answers", JSON.stringify(newAnswers)); // save the whole state in localstorage (newAnswers). "answers" is stale - not yet updated state
+  };
 
   return (
     <>
       <NavigationBar />
       <Container>
         <Row>
-          <Col
-            xs={12}
-            md={{ span: 7, offset: 2 }}
-            lg={{ span: 4, offset: 4 }}
-          >
-            {alert && <Alert onClose={() => setAlert(false)} dismissible>You successfully saved your entry! </Alert>}
+          <Col xs={12} md={{ span: 7, offset: 2 }} lg={{ span: 4, offset: 4 }}>
+            {alert && (
+              <Alert onClose={() => setAlert(false)} dismissible>
+                You successfully saved your entry!{" "}
+              </Alert>
+            )}
             <Routes>
               <Route path="*" element={<Home />} />
-              <Route path="register-meal" element={<FoodForm setAnswers={addAnswer} setAlert={setAlert} />} />
-              <Route path="register-symptoms" element={<SymptomsComp setAnswers={addAnswer} setAlert={setAlert} />} />
+              <Route
+                path="register-meal"
+                element={
+                  <FoodForm setAnswers={addAnswer} setAlert={setAlert} />
+                }
+              />
+              <Route
+                path="register-symptoms"
+                element={
+                  <SymptomsComp setAnswers={addAnswer} setAlert={setAlert} />
+                }
+              />
               <Route path="calendar" element={<Calendar answers={answers} />} />
             </Routes>
           </Col>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import foodMood from "./FOODMOOD.png"
+import foodMood from "./FOODMOOD.png";
 
 export function Home() {
-
-
   return (
     <>
-      <Container className=" my-2 d-flex flex-column justify-content-between text-center">
+      <Container className=" my-2 d-flex flex-column justify-content-between text-center align-items-stretch">
         <img src={foodMood} alt="foodmood logo"></img>
-        <Link className="btn btn-light mb-3 mt-3" to="/register-meal">Register Meal</Link>
-        <Link className="btn btn-light" to="/register-symptoms">Register Symptoms</Link>
+        <Link className="btn btn-light mb-3 mt-3" to="/register-meal">
+          Register Meal
+        </Link>
+        <Link className="btn btn-light" to="/register-symptoms">
+          Register Symptoms
+        </Link>
       </Container>
     </>
-  )
+  );
 }
