fda581bedfb3b3e1fab0041695d247b00679b3c0
Ruth Temporal <ruthtemp@gmail.com>
2022-10-20 17:27:19 +0200

    width image to 100

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,8 +5,8 @@ import foodMood from "./FOODMOOD.png";
 
 export function Home() {
   return (
-    <div className=" my-2 d-flex flex-column justify-content-between  align-items-stretch">
-      <img src={foodMood} alt="foodmood logo"></img>
+    <div className=" my-2 d-flex flex-column justify-content-between align-items-stretch">
+      <img src={foodMood} alt="foodmood logo" className="w-100"></img>
       <Link className="btn btn-light mb-3 mt-3" to="/register-meal">
         Register Meal
       </Link>
